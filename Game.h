62d8802bec5c1b1c33d8ccff94e6bc82d3f6cca5
Nicolas <Nicolas@Pc-Nicolas-Asus.educ.wifi>
2015-03-02 17:46:06 +0100

    Fill Game.h to create structure

--- Game.h
+++ Game.h
@@ -8,6 +8,19 @@
 #ifndef GAME_H_
 #define GAME_H_
 
+#include "Scene.h"
 
+
+typedef struct Game_s{
+char* name;
+Scene* scenes;
+int count_scenes;
+}Game_s;
+
+extern Game_s Game;
+
+void game_launch();
+//int game_load_scenes();
+int game_load_scene(char*);
 
 #endif /* GAME_H_ */
