f208b60a9588ceb7caedc57bb4c04980053dba9a
Gabriel <Gabriel@PC-Gabriel>
2015-03-03 16:10:27 +0100

    +add structure and prototyped for Scene.h

--- Scene.h
+++ Scene.h
@@ -8,6 +8,23 @@
 #ifndef SCENE_H_
 #define SCENE_H_
 
+#include "SceneObject.h"
+#include "Camera.h"
 
+typedef struct Scene {
+	char* name;
+	SceneObject* sceneObjects;
+	int count;
+	Script* script;
+	Camera camera;
+} Scene;
+
+Scene* scene_create();
+void scene_destroy(Scene* scene);
+void scene_setup(Scene* scene);
+void scene_run(Scene* scene);
+void scene_add_so(SceneObject* so);
+int scene_delete_so(SceneObject* so);
+SceneObject* scene_find_so(SceneObject* so);
 
 #endif /* SCENE_H_ */
