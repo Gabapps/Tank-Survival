145ceea2573fc2749dbfc74f052c9653ce622068
ethel <ethel@ETHEL-ASUS.educ.wifi>
2015-03-16 16:57:05 +0100

    Implementation of camera_refresh_matrices(Camera* cam) in Camera.C
    ++ Transform transform in Camera structure

--- Camera.c
+++ Camera.c
@@ -6,6 +6,7 @@
  */
 
 #include "Camera.h"
+#include "Transform.h"
 //==> window_getratio()pour 640.f/480.f
 
 //view et perspective sont relatives a la gestion de la camera?
@@ -13,12 +14,17 @@ void camera_init(Camera* cam){
 	cam->fov = 70.f;
 	cam->near = 0.1f;
 	cam->far = 100.f;
+	mat4x4_perspective(cam->perspective_matrix, 70.f, 640.f/480.f, 0.1f, 100.f);
 }
 
 void camera_refresh_matrices(Camera* cam){
-	//ça a pas de sens? je devrais utiliser que perspective_matrix
+
+	transform_refresh_matrix(cam->transform);
+
+	/*ça a pas de sens? je devrais utiliser que perspective_matrix
 	mat4x4 M, V, P, MVP;
 	 mat4x4_mul(MVP, V, M); // MVP = P * V * M
 	 mat4x4_mul(MVP, P, MVP);
+	 */
 }
 //
