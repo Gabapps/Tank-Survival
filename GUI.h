9f028d7f80803a540fe4bd5c1405d916c0e78abf
Gabriel <Gabriel@PC-Gabriel>
2015-04-04 17:32:54 +0200

    + add first GUI structures

---
+++ GUI.h
@@ -0,0 +1,22 @@
+/*
+ * GUI.h
+ *
+ *  Created on: 4 avr. 2015
+ *      Author: Gabriel
+ */
+
+#ifndef GUI_H_
+#define GUI_H_
+
+#include "Widget.h"
+
+typedef struct GUI_s {
+	Widget* root;
+} GUI_s;
+
+GUI_s GUI;
+
+void GUI_init();
+void GUI_draw();
+
+#endif /* GUI_H_ */
