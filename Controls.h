ec632325b07199dbac25ba476e6e095243244198
Nicolas <Nicolas@Pc-Nicolas-Asus>
2015-04-06 17:45:28 +0200

    Add Documentations for Controls and Input

--- Controls.h
+++ Controls.h
@@ -18,8 +18,31 @@ typedef struct{
 	int key_state;
 } Controls;
 
+/**
+ * \fn void init_controlsTable();
+ * \brief Allocate the Controls structure
+ */
 void init_controlsTable();
+
+/**
+ * \fn int controls_create(char* action_name, int key_number);
+ * \brief Create a control by giving a key number. Then the control is known as the action_name give as parameter.
+ *
+ * \param action_name The name of the key
+ * \param key_number The index number of the key
+ *
+ * return 0 when there is not any problems
+ */
 int controls_create(char* action_name, int key_number);
+
+/**
+ * \fn int controls_destroy(char* control_name);
+ * \brief Destroy a control from its action_name give as parameter.
+ *
+ * \param action_name The name of the key
+ *
+ * return 0 when there is not any problems
+ */
 int controls_destroy(char* control_name);
 
 #endif /* CONTROLS_H_ */
